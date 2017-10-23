c8a0d832c36c40e28a5fd66a0d3b5ef0000a7604
Ian Lehfeldt <ian.lehfeldt@gmail.com>
2017-10-22 20:46:48 -0500

    express sends a raa random number between 100 and 500

--- app.js
+++ app.js
@@ -4,18 +4,22 @@ var port = 5000;
 
 //var random = randomNumber(100,500);
 
-function randomNumber(min, max){
+function randomNumber(min, max) {
     return Math.floor(Math.random() * (1 + max - min) + min);
 }
 
 app.use(express.static('public'));
 
-app.listen(port, function() {
+app.listen(port, function () {
     console.log('Listening on port', port);
-    
+
 });
 
-app.get('/random', function(req, res){
-    var random = randomNumber(100,500);
-    res.sendStatus(random);
+app.get('/random', function (req, res) {
+    var random = {
+        number: randomNumber(100, 500)
+    }
+    //console.log('Random Number: ', random.number);
+    
+    res.send(random);
 });
